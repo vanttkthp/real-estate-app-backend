fbc2f413387a2f76ba86e9a2f48c7a8f42a77f43
vanttkthp <congvan2357@gmail.com>
2024-07-09 16:04:49 +0700

    Real-time Chat feature

--- app.js
+++ app.js
@@ -1,32 +1,26 @@
 import express from "express";
-import bodyParser from "body-parser";
-import postRoute from "./routes/post.route.js";
-import authRoute from "./routes/auth.route.js";
-import cookieParser from "cookie-parser";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoute from "./routes/auth.route.js";
+import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
-const PORT = process.env.PORT;
-
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(express.json());
 app.use(cookieParser());
-// Middleware to parse JSON bodies
-app.use(bodyParser.json());
 
-// Alternatively, if you are using Express 4.16.0 or later, you can use:
-// app.use(express.json());
-
-// Middleware to parse URL-encoded bodies
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
-app.use("/api/test", testRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/test", testRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(8080, () => {
+  console.log("Server is running!");
 });
